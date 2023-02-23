b637fd488ea01fe22dc54d3b113f9211af840f52
Parabeac-Nest <admin@parabeac.com>
2023-02-23 21:48:07 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/teigebrasil_colors.g.dart
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TeigebrasilColors {
+
+static const Color rgb(191,191,191) = Color(0xffbfbfbf);
+
+static const Color rgb(244,245,247) = Color(0xfff4f5f7);
+
+static const Color rgb(107,109,113) = Color(0xff6b6d71);
+
+static const Color rgb(255,255,255) = Color(0xffffffff);
+
+static const Color rgb(40,41,43) = Color(0xff28292b);
+
+static const Color rgb(0,77,102) = Color(0xff004d66);
+
+static const Color rgb(0,146,156) = Color(0xff00929c);
+}
